77dde80c63bfbc792f0be718adba5437d40d2345
weiyang <weiyang.ones@gmail.com>
2017-11-01 10:31:29 +0800

    [cidr] Add private networks
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ import requests
 
 cidr_url = "http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone"
 
-networks = []
+private_networks = ["10.0.0.0/8",
+                    "127.0.0.1/32",
+                    "100.64.0.0/10",
+                    "172.16.0.0/12",
+                    "192.168.0.0/16", ]
+networks = [ipaddress.IPv4Network(n) for n in private_networks]
 resp = requests.get(cidr_url)
 for line in resp.content.splitlines():
     networks.append(ipaddress.IPv4Network(line.decode("utf-8")))
