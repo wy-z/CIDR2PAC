678ba51ad8f7eafe8f805430e5c4283bc72c5231
weiyang <weiyang.ones@gmail.com>
2018-03-12 14:21:29 +0800

    Refactor
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CIDR2PAC
 Convert CN-CIDR list to PAC.
+
+## QuickStart
+
+```
+Usage: main.py [OPTIONS]
+
+Options:
+  --proxy-expr TEXT  proxy expression
+  --help             Show this message and exit.
+```
--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-
 import datetime
 import ipaddress
+import os
+from urllib import request
 
 import jinja2
-import requests
+import click
 
-cidr_url = "http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone"
+cn_cidr_url = "http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone"
 
-private_networks = ["10.0.0.0/8",
-                    "127.0.0.1/32",
-                    "100.64.0.0/10",
-                    "172.16.0.0/12",
-                    "192.168.0.0/16", ]
-networks = [ipaddress.IPv4Network(n) for n in private_networks]
-resp = requests.get(cidr_url)
-for line in resp.content.splitlines():
-    networks.append(ipaddress.IPv4Network(line.decode("utf-8")))
+private_networks = [
+    "10.0.0.0/8",
+    "127.0.0.1/32",
+    "100.64.0.0/10",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+]
 
-proxy_expr = "SOCKS5 localhost:1086"
-with open("pac.j2") as f:
-    pac = jinja2.Template(f.read()).render(networks=networks, date=datetime.datetime.now(
-        datetime.timezone.utc), proxy_expr=proxy_expr)
-    print(pac)
+
+@click.command()
+@click.option(
+    "--proxy-expr", default="SOCKS5 localhost:1086", help="proxy expression")
+def main(proxy_expr):
+    networks = [ipaddress.IPv4Network(n) for n in private_networks]
+
+    resp = request.urlopen(cn_cidr_url)
+    for line in resp.readlines():
+        cidr = line.decode("utf-8").strip()
+        networks.append(ipaddress.IPv4Network(cidr))
+
+    with open(os.path.join("template", "pac.j2")) as f:
+        pac = jinja2.Template(f.read()).render(
+            networks=networks,
+            date=datetime.datetime.now(datetime.timezone.utc),
+            proxy_expr=proxy_expr)
+        print(pac)
+
+
+if __name__ == '__main__':
+    main()
